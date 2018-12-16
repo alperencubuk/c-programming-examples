6e1454bd4556030f720ece30d7fecfb50118b041
alperencubuk <alperencubuk@outlook.com>
2018-12-16 20:03:03 +0300

    C Examples

--- 5. Hafta/Pointer İlk Örnek.c
+++ 5. Hafta/Pointer İlk Örnek.c
@@ -4,13 +4,13 @@ void fonksiyon(int);
 void pointer(int *);
 
 int main() {
-	int f=1;
+    int f=1;
     fonksiyon(f);
     printf("f: %d\n",f);
     
     int p=1;
     pointer(&p);
-	printf("p: %d\n",p);
+    printf("p: %d\n",p);
 	
     return 0;
 }

9efed64aa5cc1adba8ff87b26d274efd0539d848
alperencubuk <alperencubuk@outlook.com>
2018-12-23 17:53:32 +0300

    C Examples

--- 3. Hafta/Dizi Sıralama 2.c
+++ 3. Hafta/Dizi Sıralama 2.c
@@ -5,7 +5,7 @@ int main() {
 	int dizi[N]={1,8,4,5,9,3,7,2,6};
 	int i,j,temp; 
 	for(i=0;i<N;i++) {
-		for(j=0;j<N;j++) {
+		for(j=0;j<N-1;j++) {
 			if(dizi[j]>dizi[j+1]) {
 				temp=dizi[j];
 				dizi[j]=dizi[j+1];
